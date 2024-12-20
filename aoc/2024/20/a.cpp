ab49c1b373aa22acdbf8a36e5aad13c8a6c68d04
krypek <krypek@tuta.io>
2024-12-20 18:12:53 +0100

    Add AoC 2024 day 20 part 2

--- aoc/2024/20/a.cpp
+++ aoc/2024/20/a.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include <csignal>
-#include <stdexcept>
 using namespace std;
 
 struct Vec2 {
@@ -11,7 +10,7 @@ struct Vec2 {
 
 typedef unsigned long long ll;
 
-ll solve1(string inputFile) {
+ll solve(string inputFile, const int cheatSize) {
   ifstream cin(inputFile);
 
   vector<string> lines;
@@ -40,26 +39,23 @@ ll solve1(string inputFile) {
   assert(start.x != -1);
   assert(end.x != -1);
 
+  vector<vector<int>> distGrid(h, vector<int>(w, -1));
   auto calcNormalDist = [&]() {
-    vector<vector<bool>> visited(h, vector<bool>(w, false));
     vector<Vec2> toVisit;
     toVisit.push_back(start);
-    visited[start.y][start.x] = true;
+    distGrid[start.y][start.x] = 0;
 
     for (int dist = 0; toVisit.size() > 0; dist++) {
       vector<Vec2> nextToVisit;
       for (Vec2 pos : toVisit) {
-        if (pos == end) {
-          return dist;
-        }
         auto checkNormal = [&](int x, int y) {
           if (lines[y][x] == '#')
             return;
-          if (visited[y][x])
+          if (distGrid[y][x] != -1)
             return;
 
-          visited[y][x] = true;
           nextToVisit.push_back({x, y});
+          distGrid[y][x] = dist + 1;
         };
 
         checkNormal(pos.x + 1, pos.y);
@@ -69,14 +65,17 @@ ll solve1(string inputFile) {
       }
       toVisit = nextToVisit;
     }
-    throw invalid_argument("Normal solution not found!");
   };
-  int normalDist = calcNormalDist();
+  calcNormalDist();
+  int normalDist = distGrid[end.y][end.x];
 
   vector<vector<bool>> visited(h, vector<bool>(w, false));
   vector<Vec2> toVisit;
   toVisit.push_back(start);
   visited[start.y][start.x] = true;
+
+  multiset<int> diffs;
+  set<ll> hacks;
 
   ll sum = 0;
   for (int dist = 0; toVisit.size() > 0; dist++) {
@@ -103,75 +102,67 @@ ll solve1(string inputFile) {
       auto isOutOfBounds = [w, h](int x, int y) {
         return x < 0 || x >= w || y < 0 || y >= h;
       };
-      auto checkClip = [&](int x1, int y1, int x2, int y2, int x3, int y3) {
-        if (isOutOfBounds(x3, y3) || visited[y3][x3] || lines[y1][x1] != '#' ||
-            ((x2 != -1 && y2 != -1) && lines[y2][x2] != '#') ||
-            lines[y3][x3] == '#')
+      auto checkClip = [&](int fromX, int fromY, int toX, int toY) {
+        if (isOutOfBounds(toX, toY) || visited[toY][toX] ||
+            lines[toY][toX] == '#' || distGrid[toY][toX] == -1)
           return;
 
-        auto hackBfs = [w, h, &visited, end, &lines, &isOutOfBounds, y3, x3, x1,
-                        y1, normalDist](Vec2 start, int startDist) {
-          vector<vector<bool>> hackVisited(h, vector<bool>(w, false));
-          vector<Vec2> toVisit;
-          toVisit.push_back(start);
-          hackVisited[start.y][start.x] = true;
-
-          for (int dist = startDist; toVisit.size() > 0; dist++) {
-            vector<Vec2> nextToVisit;
-            for (Vec2 pos : toVisit) {
-              if (pos == end) {
-                return dist;
-              }
-              auto checkNormal = [&](int x, int y) {
-                assert(!isOutOfBounds(x, y));
-                if (lines[y][x] == '#')
-                  return;
-                if (visited[y][x] || hackVisited[y][x])
-                  return;
-
-                hackVisited[y][x] = true;
-                nextToVisit.push_back({x, y});
-              };
-
-              checkNormal(pos.x + 1, pos.y);
-              checkNormal(pos.x - 1, pos.y);
-              checkNormal(pos.x, pos.y + 1);
-              checkNormal(pos.x, pos.y - 1);
-            }
-            toVisit = nextToVisit;
-          }
-          return -1;
-        };
-
-        int newDist = hackBfs({x3, y3}, dist + 2);
+        int fromToToDist = abs(fromX - toX) + abs(fromY - toY);
+        int newDist = dist + fromToToDist + (normalDist - distGrid[toY][toX]);
         int diff = normalDist - newDist;
 
-        if (newDist != -1) {
+        if (diff > 0) {
+          ll en = toX + toY * w + fromX * w * w + fromY * w * w * w;
+          assert(hacks.count(en) == 0);
+          hacks.insert(en);
+
+          diffs.insert(diff);
           if (diff >= 100) {
             sum++;
           }
         }
       };
 
-      checkClip(pos.x, pos.y - 1, -1, -1, pos.x, pos.y - 2);
-      checkClip(pos.x + 1, pos.y, -1, -1, pos.x + 2, pos.y);
-      checkClip(pos.x, pos.y + 1, -1, -1, pos.x, pos.y + 2);
-      checkClip(pos.x - 1, pos.y, -1, -1, pos.x - 2, pos.y);
+      for (int oy1 = 0; oy1 < cheatSize + 1; oy1++) {
+        for (int ox = cheatSize - oy1; ox < cheatSize + oy1 + 1; ox++) {
+          if ((oy1 == cheatSize - 1 || oy1 == cheatSize ||
+               oy1 == cheatSize + 1) &&
+              (ox == cheatSize - 1 || ox == cheatSize || ox == cheatSize + 1)) {
+            continue;
+          }
+
+          if (oy1 != cheatSize) {
+            int oy2 = cheatSize * 2 - oy1;
+            checkClip(pos.x, pos.y, pos.x + ox - cheatSize,
+                      pos.y + oy2 - cheatSize);
+          }
+          checkClip(pos.x, pos.y, pos.x + ox - cheatSize,
+                    pos.y + oy1 - cheatSize);
+        }
+      }
     }
     toVisit = nextToVisit;
   }
 
 ret:
+  // set<int> printedAlready;
+  // for (int diff : diffs) {
+  //   if (printedAlready.count(diff) > 0)
+  //     continue;
+  //   printedAlready.insert(diff);
+  //
+  //   print("times: {}, saved: {}\n", diffs.count(diff), diff);
+  // }
+
   return sum;
 }
-
 int main() {
   // clang-format off
-  // printf("solve1 ex0 (0 expected): %llu\n", solve1("ex0"));
-  // printf("solve1 in0 (1289 expected): %llu\n", solve1("in0"));
+  printf("solve1 ex0 (0 expected): %llu\n", solve("ex0", 2));
+  printf("solve1 in0 (1289 expected): %llu\n", solve("in0", 2));
 
-  // printf("solve2 ex0 (0 expected): %llu\n", solve2("ex0"));
-  // printf("solve1 in0 (642535800868438 expected): %llu\n", solve1("in0", true));
+  printf("solve2 ex0 (0 expected): %llu\n", solve("ex0", 20));
+  printf("solve2 in0 (982425 expected): %llu\n", solve("in0", 20));
   // clang-format on
 
   return 0;
