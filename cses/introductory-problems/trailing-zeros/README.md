a50e390e105bcdd12ab845f81d4c77600d554017
krypek <krypek@tuta.io>
2025-01-03 12:02:37 +0100

    Add CSES trailing-zeros

---
+++ cses/introductory-problems/trailing-zeros/README.md
@@ -0,0 +1,4 @@
+# [CSES - Trailing Zeros](https://cses.fi/problemset/task/1618)
+
+Answer accepted
+
---
+++ cses/introductory-problems/trailing-zeros/a.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#ifdef DEBUG
+#include <print>
+#include <fstream>
+#include <sstream>
+#endif
+
+using namespace std;
+
+typedef long long ll;
+
+int run(istream &cin, ostream &cout) {
+	ll n;
+	cin >> n;
+
+	int zeros = 0;
+	ll div = 5;
+	while (div <= n) {
+		zeros += n / div;
+		div *= 5;
+	}
+
+	cout << zeros << endl;
+	return 0;
+}
+
+
+#ifdef DEBUG
+
+string rtrim(const string &s) {
+	size_t end = s.find_last_not_of(" \n\r\t\f\v");
+	return (end == string::npos) ? "" : s.substr(0, end + 1);
+}
+
+void tf(string name) {
+	ifstream in(format("{}.in", name));
+	ostringstream out;
+	
+	ifstream exp(format("{}.out", name));
+	string expStr(istreambuf_iterator<char>{exp}, {});
+	expStr = rtrim(expStr);
+
+	run(in, out);
+	string outStr = rtrim(out.str());
+
+	if (outStr != expStr) {
+		print("test {} failed! expected: \n'{}'\n--- got \n'{}'\n", name, expStr, outStr);
+	} else {
+		print("test {} success\n", name);
+	}
+}
+
+int main() {
+	tf("ex0");
+	tf("ex1");
+	return 0;
+}
+#else
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	return run(cin, cout);
+}
+#endif
