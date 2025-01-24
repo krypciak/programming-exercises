dd10ef3437c107c0e7c2141b3c8ead465fe14c93
krypek <krypek@tuta.io>
2025-01-24 14:23:30 +0100

    Add Szkopul dijkstra

---
+++ szkopul/dijkstra/README.md
@@ -0,0 +1,3 @@
+# [Szkopuł - Dijkstra Dijkstra](https://szkopul.edu.pl/problemset/problem/Ik13DlOhJ9ujfLr0VFoy_st4/site/?key=statement)
+
+`100` points.
---
+++ szkopul/dijkstra/a.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int n, m;
+
+struct GE {
+	ll len;
+	int to;
+	int m;
+	bool active;
+
+	bool operator<(const GE &a) const { return len < a.len; }
+};
+vector<vector<GE*>> g;
+vector<pair<int, int>> prevn;
+
+ll dij(bool hasNeg) {
+	typedef pair<ll, int> pl;
+	priority_queue<pl, vector<pl>, greater<pl>> pq;
+	pq.push({0, 1});
+	vector<ll> dist(n+1, LLONG_MAX);
+	dist[1] = 0;
+	prevn = vector<pair<int, int>>(n+1);
+	vector<bool> visited(n+1, false);
+
+	while(pq.size()) {
+		auto [l, v] = pq.top();
+		pq.pop();
+		if (!hasNeg) visited[v] = true;
+
+		if (v == n) return l;
+
+		if (dist[v] < l) continue;
+
+		for (auto ref : g[v]) {
+			auto [l1, v1, i1, active] = *ref;
+			if (!hasNeg && visited[v1]) continue;
+			if (!active) continue;
+
+			ll nl = l + l1;
+			if (nl < dist[v1]) {
+				pq.push({nl, v1});
+				dist[v1] = nl;
+				prevn[v1] = {v, i1};
+			}
+		}
+	}
+	return dist[n];
+}
+
+#ifdef TEST
+int run(istream &cin, ostream &cout) {
+#else
+int main() {
+#endif
+	ios_base::sync_with_stdio(0), cin.tie(0);
+
+	cin >> n >> m;
+	if (n == 1) {
+		cout << 0 << endl;
+		return 0;
+	}
+
+	g = vector<vector<GE*>>(n+1);
+
+	vector<GE> edges(m*2);
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		ll c;
+		cin >> a >> b >> c;
+
+		edges[i] = {c, b, i, true};
+		g[a].push_back(&edges[i]);
+
+		edges[i+m] = {-c, a, i+m, false};
+		g[b].push_back(&edges[i+m]);
+	}
+
+	if (dij(false) == LLONG_MAX) { cout << -1 << endl; return 0; }
+
+	vector<int> pOrig;
+	{
+		pOrig.reserve(n);
+		int v = n;
+		while(v != 1) {
+			int i = prevn[v].second;
+			pOrig.push_back(i);
+			assert(i < m);
+
+			edges[i].active = false;
+			edges[i+m].active = true;
+			
+			v = prevn[v].first;
+		}
+		reverse(pOrig.begin(), pOrig.end());
+	}
+
+	if (dij(true) == LLONG_MAX) { cout << -1 << endl; return 0; }
+	vector<int> pNew;
+	{
+		pNew.reserve(n);
+		int v = n;
+		while(v != 1) {
+			int i = prevn[v].second;
+			pNew.push_back(i);
+
+			v = prevn[v].first;
+		}
+		reverse(pNew.begin(), pNew.end());
+	}
+
+	vector<int> inter(m, 0);
+	for (auto v : pOrig) inter[v]++;
+	for (auto v : pNew) { if (v >= m) v -= m; inter[v]++; }
+
+	vector<pair<int, ll>> out1;
+	out1.push_back({edges[pOrig[0]].to, edges[pOrig[0]].len});
+	out1.push_back({edges[pNew[0]].to, edges[pNew[0]].len});
+
+	ll sum = 0;
+	for (auto [v, len] : out1) {
+		while (v != n) {
+			for (auto r : g[v]) {
+				if (r->m >= m || inter[r->m] != 1) continue;
+				v = r->to;
+				len += r->len;
+				break;
+			}
+		}
+		sum += len;
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
+#ifdef TEST
+string rtrim(string s) {
+	auto end = s.find_last_not_of(" \n\r\t\f\v");
+	return end == string::npos ? s : s.substr(0, end+1);
+}
+void printT(string name, string outS, string expS) {
+	if (outS == expS) {
+		print("\u001b[32m\u001b[1m\u221A\u001b[0m\u001b[22m {} success\n", name);
+	} else {
+		print("\u001b[31m\u001b[1mx\u001b[0m\u001b[22m {} fail! expected: \n'{}'\n---got: \n'{}'\n\n", name, expS, outS);
+	}
+}
+void test(string name) {
+	ifstream in(name+".in");
+	ostringstream out;
+
+	run(in, out);
+	string outS = rtrim(out.str());
+
+	ifstream exp(name+".out");
+	string expS(istreambuf_iterator<char>{exp}, {});
+	expS = rtrim(expS);
+
+	printT(name, outS, expS);
+}
+int main() {
+	test("ex0");
+	test("ex1");
+	return 0;
+}
+#endif
