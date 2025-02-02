fef72ebcffb6e7367e177cf523a97d4a4e94ee15
krypek <krypek@tuta.io>
2025-02-02 15:36:37 +0100

    Add OKI poziom 2 2024/2025 mal

---
+++ oki/2024-2025/poziom2/lca/mal/README.md
@@ -0,0 +1,3 @@
+# [OKI poziom 2 2024/2025 - Mały młot](https://szkopul.edu.pl/problemset/problem/-WqVHJz23rlsUXmiBl81poIu/site/?key=statement)
+
+`100` points.
---
+++ oki/2024-2025/poziom2/lca/mal/a.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+#ifdef TEST
+int run(istream &cin, ostream &cout) {
+#else
+int main() {
+#endif
+	ios_base::sync_with_stdio(0), cin.tie(0);
+
+	int n;
+	vector<vector<int>> G;
+	const int MH = 21;
+	vector<vector<int>> JP;
+
+	cin >> n;
+	G.assign(n+1, {});
+	for (int i = 0; i < n-1; i++) {
+		int a, b;
+		cin >> a >> b;
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+	JP.assign(MH, vector<int>(n+1, 0));
+	vector<int> V{1};
+	JP[0][1] = 0;
+	vector<bool> visited(n+1, false);
+	visited[1] = true;
+	while(V.size()) {
+		vector<int> NV;
+		for (int v : V) for (int u : G[v]) {
+			if (visited[u]) continue;
+			visited[u] = true;
+			JP[0][u] = v;
+			NV.push_back(u);
+		}
+		V = NV;
+	}
+	for (int deg = 1; deg < MH; deg++)
+		for (int v = 0; v <= n; v++)
+			JP[deg][v] = JP[deg-1][JP[deg-1][v]];
+	int q;
+	cin >> q;
+	while(q--) {
+		int v, deg;
+		cin >> v >> deg;
+		deg--;
+
+		int res = JP[deg][v];
+		if (res == 0) cout << "LUCY" << '\n';
+		else cout << res << '\n';
+	}
+	return 0;
+}
+#ifdef TEST
+string rtrim(string s) {
+	auto end = s.find_last_not_of(" \n\r\t\f\v");
+	return end == string::npos ? s : s.substr(0, end+1);
+}
+void printT(string name, string outS, string expS) {
+	if (outS == expS) {
+		print("\u001b[32m\u001b[1m\u221A\u001b[0m\u001b[22m {} success\n", name);
+	} else {
+		print("\u001b[31m\u001b[1mx\u001b[0m\u001b[22m {} fail! expected: \n'{}'\n---got: \n'{}'\n\n", name, expS, outS);
+	}
+}
+void test(string name) {
+	ifstream in(name+".in");
+	ostringstream out;
+
+	run(in, out);
+	string outS = rtrim(out.str());
+
+	ifstream exp(name+".out");
+	string expS(istreambuf_iterator<char>{exp}, {});
+	expS = rtrim(expS);
+
+	printT(name, outS, expS);
+}
+int main() {
+	test("ex0");
+	test("ex1");
+	return 0;
+}
+#endif
