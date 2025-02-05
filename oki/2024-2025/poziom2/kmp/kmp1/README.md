200b802d9c50c96ad40f3c2493e5322f264c9d43
krypek <krypek@tuta.io>
2025-02-05 16:41:54 +0100

    Add OKI poziom 2 2024/2025 kmp1

---
+++ oki/2024-2025/poziom2/kmp/kmp1/README.md
@@ -0,0 +1,3 @@
+# [OKI poziom 2 2024/2025 - KMP](https://szkopul.edu.pl/problemset/problem/sqgZus_Bk58dqpzm-_42eibq/site/?key=statement)
+
+`100` points.
---
+++ oki/2024-2025/poziom2/kmp/kmp1/a.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifdef TEST
+int run(istream &cin, ostream &cout) {
+#else
+int main() {
+#endif
+	ios_base::sync_with_stdio(0), cin.tie(0);
+
+	int n;
+	string s;
+	cin >> n >> s;
+
+	vector<int> kmp(n+1);
+	kmp[0] = -1;
+	int t = -1;
+
+	for (int i = 1; i <= n; i++) {
+		while (t >= 0 and s[t] != s[i-1]) t = kmp[t];
+		kmp[i] = ++t;
+	}
+	for (int i = 1; i <= n; i++) cout << kmp[i] << " ";
+	cout << endl;
+
+	return 0;
+}
+#ifdef TEST
+string rtrim(string s) {
+	auto end = s.find_last_not_of(" \n\r\t\f\v");
+	return end == string::npos ? s : s.substr(0, end+1);
+}
+void printT(string name, string outS, string expS) {
+	if (outS == expS) {
+		print("\u001b[32m\u001b[1m\u221A\u001b[0m\u001b[22m {} success\n", name);
+	} else {
+		print("\u001b[31m\u001b[1mx\u001b[0m\u001b[22m {} fail! expected: \n'{}'\n---got: \n'{}'\n\n", name, expS, outS);
+	}
+}
+void test(string name) {
+	ifstream in(name+".in");
+	ostringstream out;
+
+	run(in, out);
+	string outS = rtrim(out.str());
+
+	ifstream exp(name+".out");
+	string expS(istreambuf_iterator<char>{exp}, {});
+	expS = rtrim(expS);
+
+	printT(name, outS, expS);
+}
+int main() {
+	test("ex0");
+	test("ex1");
+	return 0;
+}
+#endif
