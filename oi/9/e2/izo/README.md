6633a835dafac7568b163252c32b7347d88aae9b
krypek <krypek@tuta.io>
2025-02-08 13:01:44 +0100

    Add OI 9 e2 izo

---
+++ oi/9/e2/izo/README.md
@@ -0,0 +1,3 @@
+# [OI 9 e2 - Izolator](https://szkopul.edu.pl/problemset/problem/C-avIJ9h36gThNQUOeaYwAD9/site/?key=statement)
+
+`100` points.
---
+++ oi/9/e2/izo/a.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifdef TEST
+int run(istream &cin, ostream &cout) {
+#else
+int main() {
+#endif
+	ios_base::sync_with_stdio(0), cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> inp(n);
+	for (int i = 0; i < n; i++) cin >> inp[i];
+	sort(begin(inp), end(inp));
+
+	ll sum = 0;
+	for (int i = 0; i < n/2; i++) {
+		sum += inp[n-i-1] - inp[i];
+	}
+	for (int i = 0; i < n; i++) sum += inp[i];
+	cout << sum;
+	
+	return 0;
+}
+#ifdef TEST
+string rtrim(string s) {
+	auto end = s.find_last_not_of(" \n\r\t\f\v");
+	return end == string::npos ? s : s.substr(0, end+1);
+}
+void printT(string name, string outS, string expS) {
+	if (outS == expS) {
+		print("\u001b[32m\u001b[1m\u221A\u001b[0m\u001b[22m {} success\n", name);
+	} else {
+		print("\u001b[31m\u001b[1mx\u001b[0m\u001b[22m {} fail! expected: \n'{}'\n---got: \n'{}'\n\n", name, expS, outS);
+	}
+}
+void test(string name) {
+	ifstream in(name+".in");
+	ostringstream out;
+
+	run(in, out);
+	string outS = rtrim(out.str());
+
+	ifstream exp(name+".out");
+	string expS(istreambuf_iterator<char>{exp}, {});
+	expS = rtrim(expS);
+
+	printT(name, outS, expS);
+}
+int main() {
+	test("ex0");
+	return 0;
+}
+#endif
