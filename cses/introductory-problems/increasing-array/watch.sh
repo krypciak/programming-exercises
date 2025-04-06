e8510a15a2891eeda57f1e3e9588fc58ef32f2ca
krypek <krypek@tuta.io>
2025-04-06 17:08:52 +0200

    Add CSES increasing-array

---
+++ cses/introductory-problems/increasing-array/watch.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+comp() {
+    clear
+    echo "compiling..."
+    g++ a.cpp -o a.e -Wall -DDEBUG -std=c++23 -g -fsanitize=address,undefined,leak,float-divide-by-zero,float-cast-overflow && echo "-----" && ./a.e
+}
+
+export -f comp
+
+ls -1 a.cpp | entr sh -c comp
