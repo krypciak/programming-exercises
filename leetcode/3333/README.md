752a5b235f53bf15a443c6b7e4190e0837c3d245
krypek <krypek@tuta.io>
2025-07-03 17:00:19 +0200

    Add leetcode 3333

---
+++ leetcode/3333/README.md
@@ -0,0 +1,2 @@
+# [Leetcode - 3333. Find the K-th Character in String Game II](https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii)
+
---
+++ leetcode/3333/watch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+comp() {
+	clear
+	echo compiling...
+	g++ a.cpp -o a.e -std=c++23 -DTEST -g -Wall -Wextra -fsanitize=address,leak,undefined,float-divide-by-zero,float-cast-overflow && echo ------ && ./a.e && echo .
+}
+export -f comp
+ls a.cpp | entr sh -c comp
