44fc4868e44e45daad771a1b602fe3c2fdd90f9b
krypek <krypek@tuta.io>
2025-11-21 16:51:06 +0100

    Add OI 33 e1 szkolny wyr

---
+++ oi/33/e1szkolny/wyr/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+export MEM_LIMIT=1024M
+TIME_LIMIT=3000
+max_time=0
+max_time_test=""
+g++ a.cpp -o b.e -std=c++23 -O2
+for f in **/**.in; do
+    time="$(oiejq ./b.e < "$f"  2>&1 1>/dev/null | grep "Time" | awk '{print $2}' | head --bytes -3)"
+    echo $f $time
+    if [ "$time" -gt "$max_time" ]; then
+        max_time="$time"
+        max_time_test="$f"
+    fi
+    if [ "$time" -ge "$TIME_LIMIT" ]; then
+        echo "time exceeded!" $f $time
+        exit 1
+    fi
+done
+echo "all passed, max time: $max_time on $max_time_test"
+rm -f ./b.e
+
